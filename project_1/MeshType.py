77c107ce5dcdb0cdf70ccab03604df3238ea936c
Zack <rtaylo45@vols.utk.edu>
2019-01-17 08:56:37 -0500

    Added function to build nodes from MeshType. Began to add function creating nodal connections. Added method to apply BCs.

--- project_1/MeshType.py
+++ project_1/MeshType.py
@@ -19,7 +19,8 @@ class Mesh(object):
         self.yLength = xLength
         self.numOfxNodes = xNodes
         self.numOfyNodes = yNodes
-        self.Nodes = []
+        self.numOfTotalNodes = xNodes*yNodes
+        self.nodes = []
         self.dx = None
         self.dy = None
 
@@ -27,7 +28,7 @@ class Mesh(object):
         self.__buildGeo()
         
 
-    def __self.buildGeo(self):
+    def __buildGeo(self):
 
         self.dx = self.xLength/(self.numOfxNodes-1)
         self.dy = self.yLength/(self.numOfyNodes-1)
@@ -36,27 +37,115 @@ class Mesh(object):
         self.__connectNodes()
 
     def __createNodes(self):
-        pass
+
+        absoluteIndex = 0
+       
+        # Loops from bottom left hand corner of mesh to top of mesh. Then goes
+        # to next x index. 
+        for i in xrange(self.numOfxNodes):
+            for j in xrange(self.numOfyNodes):
+                nodeObj = Node(i, j, absoluteIndex)
+                self.nodes.append(nodeObj)
+                absoluteIndex += 1
 
     def __connectNodes(self):
-        pass
+        for node in self.nodes:
+            # First column. West BC
+            if node.i == 0:
+                node.approx = self.westBC
+                node.solved = True
+
+            # Last column. East BC
+            elif node.i == self.numOfxNodes-1:
+                node.approx = self.eastBC
+                node.solved = True
+
+            # First row. South BC
+            elif node.j == 0:
+                node.approx = self.southBC 
+                node.solved = True 
+
+            # Last Row. North BC
+            elif node.j == self.numOfyNodes-1:
+                node.approx = self.northBC
+                node.solved = True
+
+            else:
+                print "Connection gone wrong"
 
 
+    def setBC(side, BC):
+        if side == "north":
+            self.northBC = BC
+        elif side == "south":
+            self.southBC = BC
+        elif side == "east":
+            self.eastBC = BC
+        elif side == "west":
+            self.westBC = BC
+        else:
+            print "Invalid BC"
+                 
 class Node(Mesh):
 
     """
     @param i    x index
     @param j    y index
     """
-    def __init__(self, i, j)
+    def __init__(self, i, j, absIndex):
         self.i = i
         self.j = j
+        self.absIndex = absIndex
         self.approx = 0.0
         self.exact = 0.0
         self.error = 0.0
+        self.solved = False
 
         # Node connection information
         self.east = None
         self.west = None
         self.north = None
         self.south = None
+
+    """
+    @param location Location of node
+    @param node     Node object
+
+    Location index:
+                    north = 0
+                    south = 1
+                    east  = 2
+                    west  = 3
+    """
+    def connect(self,location,node):
+        if location == 0:
+            self.north = node
+        elif location == 1:
+            self.south = node
+        elif location == 2:
+            self.east = node
+        elif location == 3:
+            self.west = node
+        else:
+            print "Invalid location"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ project_1/Test.py
@@ -0,0 +1,5 @@
+import MeshType as Me
+
+mesh = Me.Mesh(15,20,4,4)
+
+
