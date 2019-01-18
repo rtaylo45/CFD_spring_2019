0570e8bf1eaaacb7140fca1065a9b32b7fdeef63
Zack Taylor <rtaylo45@vols.utk.edu>
2019-01-18 10:30:46 -0500

    Finished nodal connection function in MeshType. It has also been tested

--- project_1/MeshType.py
+++ project_1/MeshType.py
@@ -4,11 +4,18 @@ Date: 1/15/19
 
 Mesh class used to generate and house geometry data.
 Node class used to house nodal information
+
+Note on the Mesh Class: The general convenction is to loop from x = 0, y to ymax.
+and so on. In the apply BC method its difference to get the 100 BC to include the
+corners of the domain. 
+
 """
 
 class Mesh(object):
 
     """
+    @brief Initializes the Mesh object. Serves as a data structure.
+
     @param xLength  Total length in x direction
     @param yLength  Total length in y direction
     @param xNodes   Number of nodes in x direction
@@ -16,26 +23,35 @@ class Mesh(object):
     """
     def __init__(self, xLength, yLength, xNodes, yNodes):
         self.xLength = xLength
-        self.yLength = xLength
+        self.yLength = yLength
         self.numOfxNodes = xNodes
         self.numOfyNodes = yNodes
         self.numOfTotalNodes = xNodes*yNodes
         self.nodes = []
         self.dx = None
         self.dy = None
+        self.northBC = None
+        self.southBC = None
+        self.eastBC = None
+        self.westBC = None
 
         # builds the geometry
         self.__buildGeo()
         
-
+    """
+    @Brief Builds the Gemoetry and sets dx and dy
+    """
     def __buildGeo(self):
 
-        self.dx = self.xLength/(self.numOfxNodes-1)
-        self.dy = self.yLength/(self.numOfyNodes-1)
+        self.dx = self.xLength/(self.numOfxNodes-1.0)
+        self.dy = self.yLength/(self.numOfyNodes-1.0)
 
         self.__createNodes()
         self.__connectNodes()
 
+    """
+    @brief Builds the nodes and puts them in mesh data continer
+    """
     def __createNodes(self):
 
         absoluteIndex = 0
@@ -44,37 +60,90 @@ class Mesh(object):
         # to next x index. 
         for i in xrange(self.numOfxNodes):
             for j in xrange(self.numOfyNodes):
-                nodeObj = Node(i, j, absoluteIndex)
+                x = i*self.dx
+                y = j*self.dy
+                nodeObj = Node(i, j, absoluteIndex, x, y)
                 self.nodes.append(nodeObj)
                 absoluteIndex += 1
 
+    """
+    @Brief Makes the node connections
+    """
     def __connectNodes(self):
-        for node in self.nodes:
-            # First column. West BC
-            if node.i == 0:
-                node.approx = self.westBC
-                node.solved = True
+        for i in xrange(self.numOfxNodes):
+            for j in xrange(self.numOfyNodes):
 
-            # Last column. East BC
-            elif node.i == self.numOfxNodes-1:
-                node.approx = self.eastBC
-                node.solved = True
+                node = self.getNodeByLoc(i,j)
 
-            # First row. South BC
-            elif node.j == 0:
-                node.approx = self.southBC 
-                node.solved = True 
+                node.north = self.getNodeByLoc(i,j+1)
+                node.south = self.getNodeByLoc(i,j-1)
+                node.west = self.getNodeByLoc(i-1,j)
+                node.east = self.getNodeByLoc(i+1,j)
 
-            # Last Row. North BC
-            elif node.j == self.numOfyNodes-1:
-                node.approx = self.northBC
-                node.solved = True
+    """
+    @Brief Sets the boundary conditions for each face
+    """
+    def __applyBC(self):
+        for j in xrange(self.numOfyNodes):
+            for i in xrange(self.numOfxNodes):
 
-            else:
-                print "Connection gone wrong"
+                node = self.getNodeByLoc(i,j)
+                # First row. South BC
+                if j == 0:
+                    node.solution = self.southBC 
+                    node.solved = True 
 
+                # Last Row. North BC
+                elif j == self.numOfyNodes-1:
+                    node.solution = self.northBC
+                    node.solved = True
 
-    def setBC(side, BC):
+                # First column. West BC
+                elif i == 0:
+                    node.solution = self.westBC
+                    node.solved = True
+
+                # Last column. East BC
+                elif i == self.numOfxNodes-1:
+                    node.solution = self.eastBC
+                    node.solved = True
+            
+                else:
+                    pass
+
+    """
+    @Brief Checks the i,j index to make sure they are in range
+
+    @param i    x index
+    @param j    y index
+    """
+    def __checkij(self,i,j):
+        # Check to make sure i index is in range
+        if i < 0:
+            return False
+        elif i >= self.numOfxNodes:
+            return False
+        # Check to see if j index is in range
+        elif j < 0:
+            return False
+        elif j >= self.numOfyNodes:
+            return False
+        else:
+            return True
+
+    """
+    @Brief Runs functions prier to solve
+    """
+    def finalize(self):
+        self.__applyBC()
+
+    """
+    @Brief Setter for boundary condition
+
+    @param side     Face where BC is applied
+    @param BC       The boundary condition value
+    """
+    def setBC(self, side, BC):
         if side == "north":
             self.northBC = BC
         elif side == "south":
@@ -85,20 +154,42 @@ class Mesh(object):
             self.westBC = BC
         else:
             print "Invalid BC"
-                 
-class Node(Mesh):
 
     """
+    @Brief Returns node object 
+
     @param i    x index
     @param j    y index
     """
-    def __init__(self, i, j, absIndex):
+    def getNodeByLoc(self, i, j):
+
+        if self.__checkij(i,j):
+            k = j + i*self.numOfyNodes
+            node = self.nodes[k]
+            return node
+        else:
+            return None
+
+class Node(Mesh):
+
+    """
+    @Brief Initilizes the node class
+
+    @param i        x index
+    @param j        y index
+    @param absIndex Abosolute index
+    @param x        x location on domain
+    @param y        y location on domain
+    """
+    def __init__(self, i, j, absIndex, x, y):
         self.i = i
         self.j = j
         self.absIndex = absIndex
-        self.approx = 0.0
-        self.exact = 0.0
-        self.error = 0.0
+        self.x = x
+        self.y = y
+        self.solution = None
+        self.exact = None
+        self.error = None
         self.solved = False
 
         # Node connection information
@@ -108,6 +199,8 @@ class Node(Mesh):
         self.south = None
 
     """
+    @Brief Sets the node connection
+
     @param location Location of node
     @param node     Node object
 
--- project_1/Test.py
+++ project_1/Test.py
@@ -1,5 +1,38 @@
 import MeshType as Me
 
-mesh = Me.Mesh(15,20,4,4)
+mesh = Me.Mesh(xLength=15, yLength=20, xNodes=4, yNodes=5)
 
 
+mesh.setBC(side="south", BC=0.)
+mesh.setBC(side="east", BC=0.)
+mesh.setBC(side="west", BC=0.)
+mesh.setBC(side="north", BC=100.)
+
+mesh.finalize()
+"""
+for i in xrange(mesh.numOfxNodes):
+	for j in xrange(mesh.numOfyNodes):
+		node = mesh.getNodeByLoc(i,j)
+		print 
+		print "Self ",i,j
+		try:
+			print "north",node.north.i,node.north.j
+		except:
+			print
+			pass
+		try:
+			print "south", node.south.i,node.south.j
+		except:
+			print 
+			pass
+		try:
+			print "east", node.east.i,node.east.j
+		except:
+			print 
+			pass
+		try:
+			print "west", node.west.i,node.west.j
+		except:
+			print
+			pass
+"""
